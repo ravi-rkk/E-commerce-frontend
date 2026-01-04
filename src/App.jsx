66f0f1b6198a7254ae9d412a5831d4274466b255
unknown <rravilesh@gmail.com>
2026-01-04 16:38:49 +0530

    creating forntend of e-commerce webiste vite and tailwind succesfully integrated

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h>
+        E-commerce-website
+      </h>
+    </>
+  )
+}
+
+export default App
